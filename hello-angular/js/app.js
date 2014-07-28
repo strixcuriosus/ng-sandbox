06c227add7640f626669350b6e24dc844c7824b3
Ash Hoover <strixcuriosus@gmail.com>
2014-07-27 21:35:47 -0700

    Creates myApp module

--- hello-angular/js/app.js
+++ hello-angular/js/app.js
@@ -1,12 +1,14 @@
-function ClockController($scope){
-  $scope.clock = {
-    time: new Date()
-  };
-  var refreshClock = function(){
-    $scope.clock.time = new Date();
-  };
-  setInterval(function(){
-    $scope.$apply(refreshClock); 
-  }, 1000);
-  refreshClock();
-} 
+angular.module('myApp', [])
+ .controller('ClockController', 
+    function($scope){
+      $scope.clock = {
+        time: new Date()
+      };
+      var refreshClock = function(){
+        $scope.clock.time = new Date();
+      };
+      setInterval(function(){
+        $scope.$apply(refreshClock); 
+      }, 1000);
+      refreshClock();
+    }); 
